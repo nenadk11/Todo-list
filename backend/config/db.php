a2518f96257901619c780631a30512f54a4b0aef
Nenad <sone.krivokapic@gmail.com>
2026-02-02 22:45:26 +0100

    Add database config and PDO connection

---
+++ backend/config/db.php
@@ -0,0 +1,13 @@
+<?
+$host = "localhost";
+$dbname = "task_list";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+}catch(PDOException $e){
+    die("Konekcija nije uspela: " . $e->getMessage());
+}
+?> 
