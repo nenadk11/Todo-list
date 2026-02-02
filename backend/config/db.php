2328fcb316e2fd8d48fa05c10bfc70c5f6e53316
Nenad <sone.krivokapic@gmail.com>
2026-02-02 23:19:02 +0100

    Update database error to English

--- backend/config/db.php
+++ backend/config/db.php
@@ -8,6 +8,6 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 }catch(PDOException $e){
-    die("Konekcija nije uspela: " . $e->getMessage());
+    die("Connection was unsuccessful: " . $e->getMessage());
 }
 ?> 
