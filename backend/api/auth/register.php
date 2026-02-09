0e88ce013cb65fc8292ba850ad5a0de7e80ef51d
Nenad <sone.krivokapic@gmail.com>
2026-02-09 18:22:54 +0100

    Implement user registration frontend and backend

---
+++ backend/api/auth/register.php
@@ -0,0 +1,36 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
+require_once __DIR__ . '/../../config/db.php';
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+$username = trim($data["username"]);
+$email = trim($data["email"]);
+$password = trim($data["password"]);
+
+//Provera da li je username prazan ili ima space
+if(empty($username) || preg_match('/\s/', $username)){
+    echo json_encode(["success" => false, "message" => "Username cannot be empty or contain spaces"]);
+    exit;
+}
+
+//Provera da li je email ili sifra prazna
+if(empty($email) || empty($password)){
+    echo json_encode(["success" => false, "message" => "Email and password required"]);
+    exit;
+}
+
+//Hashovanje sifre
+$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+//Ubacivanje u bazu
+$stmt = $pdo->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+
+if($stmt->execute([$username, $email, $hashedPassword])){
+    echo json_encode(["success" => true]);
+}else {
+    echo json_encode(["success" => false, "message" => "User already exists"]);
+}
+?>
---
+++ frontend/js/register.js
@@ -0,0 +1,43 @@
+const form = document.getElementById("registerForm");
+
+form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const username = form.elements["name"].value.trim();
+    const email = form.elements["email"].value.trim();
+    const password = form.elements["password"].value.trim();
+
+    if(!username || !email || !password){
+        alert("All fields are required");
+        return;
+    }
+
+    if (/\s/.test(username)) {
+        alert("Username cannot contain spaces");
+        return;
+    }
+
+    //Slanje inputa backendu
+    try {
+        const res = await fetch("http://localhost:8000/backend/api/auth/register.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ username, email, password})
+        });
+
+        const data = await res.json();
+
+        if(data.success){
+            alert("You registered successfully");
+            window.location.href = "login.html";
+        } else {
+            alert(data.message || "Unsuccessful register");
+        }
+    } catch (err) {
+        console.error(err);
+        alert("Something went wrong. Please try again.");
+    }
+});
